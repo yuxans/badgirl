004efc764f032cb1b96c5a87dac5aeaedcb54727
FKtPp <kai.fktpp@gmail.com>
2005-02-23 04:19:33 +0000

    It's real authur

--- RssQuery.py
+++ RssQuery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2005 by FKtPp
+# Copyright (C) 2005 by baa
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
