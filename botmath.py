85a370327afdea630a0441669eee38f0e4daf1cf
FKtPp <kai.fktpp@gmail.com>
2005-03-01 14:18:19 +0000

    There's some codes lie in moobot's cvs server, but not provide by the latest release version. Now they are here.

--- botmath.py
+++ botmath.py
@@ -9,7 +9,7 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
@@ -21,80 +21,81 @@ from moobot_module import MooBotModule
 handler_list=["math"]
 
 class math(MooBotModule):
-	"""Parses math expressions and returns the result"""
-	def __init__(self):
-		# Comment for the regexes:
+    """Parses math expressions and returns the result"""
+    def __init__(self):
+        # Comment for the regexes:
 
-		# Numbers:
-		# \.\d+ : a number like ".023"
-		# | : or
-		# \d+(\.\d*)? : numbers like "1.23" or "10." or "1"
-		number = "(\.\d+|\d+(\.\d*)?)"
+        # Numbers:
+        # \.\d+ : a number like ".023"
+        # | : or
+        # \d+(\.\d*)? : numbers like "1.23" or "10." or "1"
+        # L? : ends in L or not
+        number = "(\.\d+|\d+(\.\d*)?)L?"
 
-		# Operands:
-		# \(* : some or no leading parentheses
-		# -? : negative (or not)
-		#
-		# number goes here
-		#
-		# \)* : some or no trailing parentheses
-		operand = "(\(*-?" + number + "\)*)"
+        # Operands:
+        # \(* : some or no leading parentheses
+        # -? : negative (or not)
+        #
+        # number goes here
+        #
+        # \)* : some or no trailing parentheses
+        operand = "(\(*-?" + number + "\)*)"
 
-		# Operators:
-		# [ : one of these single char operators
-		#	% : modulus
-		#	+ : addition
-		#	- : subtraction
-		#	/ : division
-		#	* : multiplication
-		# ]| : or
-		# \*\* : exponentiation
-		operator = "([%+-/*]|\*\*)"
-		
-		# Total regex:
-		#
-		# ^ : starts with
-		# \s* : any amount of space, or none at all
-		#
-		# some operand
-		#
-		# \s* : some or no space after this
-		# ( : and any number of operator/operand pairs after this
-		#
-		#	operator goes here
-		#
-		#	\s* : some or no spacing
-		#
-		#	operand goes here
-		#
-		# 	\s* : any amount of trailing space, or none at all
-		# )+ : one or more operator/number pairs
-		# $ : must end here as well
-		self.regex = "^\s*" + operand + "\s*(" + operator + "\s*" + operand + "\s*)+$"
-	def handler(self, **args):
-		from irclib import Event, nm_to_n
-		# Set target
-		target = self.return_to_sender(args)
-		
-		# Split on spaces and then join everything after the first space
-		# together -- this removes the botname
-		expression = args["text"].split(" ")
-		string = ""
-		for part in expression[1:]:
-			string += part
+        # Operators:
+        # [ : one of these single char operators
+        #   % : modulus
+        #   + : addition
+        #   - : subtraction
+        #   / : division
+        #   * : multiplication
+        # ]| : or
+        # \*\* : exponentiation
+        operator = "([%+-/*]|\*\*)"
+        
+        # Total regex:
+        #
+        # ^ : starts with
+        # \s* : any amount of space, or none at all
+        #
+        # some operand
+        #
+        # \s* : some or no space after this
+        # ( : and any number of operator/operand pairs after this
+        #
+        #   operator goes here
+        #
+        #   \s* : some or no spacing
+        #
+        #   operand goes here
+        #
+        #   \s* : any amount of trailing space, or none at all
+        # )+ : one or more operator/number pairs
+        # $ : must end here as well
+        self.regex = "^\s*" + operand + "\s*(" + operator + "\s*" + operand + "\s*)+$"
+    def handler(self, **args):
+        from irclib import Event
+        # Set target
+        target = self.return_to_sender(args)
+        
+        # Split on spaces and then join everything after the first space
+        # together -- this removes the botname
+        expression = args["text"].split(" ")
+        string = ""
+        for part in expression[1:]:
+            string += part
 
-		try:
-			result = str(eval(string))
-			if len(result) > 512:
-				result = "I know the answer, but it's too long to say!"
-		except OverflowError:
-			result = "I don't have that many fingers and toes!"
-		except ZeroDivisionError:
-			result = "You can't divide by zero, stupid!"
-		except FloatingPointError:
-			result = "Floating point error."
-		except ArithmeticError:
-			result = "Arithmetic error."
-		except SyntaxError:
-			result = "Syntax error (remove leading 0's from non-octal numbers, and make sure parentheses are paired properly)"
-		return Event("privmsg", "", target, [result])
+        try:
+            result = str(eval(string))
+            if len(result) > 512:
+                result = "I know the answer, but it's too long to say!"
+        except OverflowError:
+            result = "I don't have that many fingers and toes!"
+        except ZeroDivisionError:
+            result = "You can't divide by zero, stupid!"
+        except FloatingPointError:
+            result = "Floating point error."
+        except ArithmeticError:
+            result = "Arithmetic error."
+        except SyntaxError:
+            result = "Syntax error (remove leading 0's from non-octal numbers, and make sure parentheses are paired properly)"
+        return Event("privmsg", "", target, [result])
--- bottime.py
+++ bottime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright (c) 2002 Brad Stewart
+# Portions taken from Joe Wreschnig's DateTime.pm for Funbot.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,52 +20,109 @@
 
 """bottime.py - time-related modules """
 from moobot_module import MooBotModule
-handler_list=["cputime", "uptime"]
+handler_list=["cputime", "uptime", "date", "ddate", "xday"]
 
 class cputime(MooBotModule):
 	def __init__(self):
 		self.regex = "^cputime$"
 
 	def handler(self, **args):
-		"""Reports CPU time usage for the Moobot"""
+		"""Reports CPU time usage for the current thread."""
 		from os import times
 		from irclib import Event
 		target=args["channel"]
 		if args["type"] == "privmsg":
 			from irclib import nm_to_n
 			target=nm_to_n(args["source"])
-		return Event("privmsg", "", target, [ "User time: " + str(times()[0]) + " seconds, system time: " + str(times()[1]) + " seconds.  Childrens' user time: " + str(times()[2]) + ", childrens' system time: " + str(times()[3])])
+		return Event("privmsg", "", target,
+			     ["User time: " + str(times()[0]) + " seconds, " + \
+			      "system time: " + str(times()[1]) + " seconds.  "+\
+			      "Childrens' user time: " + str(times()[2]) + ", "+\
+			      "childrens' system time: " + str(times()[3])])
 
 class uptime(MooBotModule):
 	def __init__(self):
 		import os, database, time
 		self.regex = "^uptime$"
-		self.ppid = os.getppid()
+		self.pid = os.getpid()
 
-		database.doSQL("delete from data where type = 'uptime' and created_by != '" + str(os.getppid()) + "'");
-		if len(database.doSQL("select * from data where type = 'uptime' and created_by ='" + str(os.getppid()) + "'")) == 0:
-			database.doSQL("insert into data values('" + str(int(time.time())) + "', 'uptime', '" + str(os.getppid()) + "')")
+		database.doSQL("DELETE FROM data WHERE type = 'uptime' AND " + \
+			       "created_by != '" + str(self.pid) + "'");
+		if len(database.doSQL("SELECT * FROM data WHERE " + \
+				      "type = 'uptime' AND " + \
+				      "created_by ='" + str(self.pid) + "'")) == 0:
+			database.doSQL("INSERT INTO data (data, type, created_by) "+\
+				       "VALUES('" + str(int(time.time())) + "', "+\
+				       "'uptime', '" + str(self.pid) + "')")
 
 	def handler(self, **args):
-		import database, os, time
+		import database
 		from irclib import Event
-		start_time = database.doSQL("select data from data where type = 'uptime' and created_by ='" + str(os.getppid()) + "'")[0][0]
-		uptime = int(time.time()) - int(start_time)
-		result = "I've been awake "
-		if uptime > 86400:  # seconds/day
-			days = uptime / 86400
-			result += str(days) +" days, "
-			uptime %= 86400
-		if uptime > 3600: # seconds/hour
-			hours = uptime / 3600
-			result += str(hours) +" hours, "
-			uptime %= 3600
-		if uptime > 60: 
-			hours = uptime / 60
-			result += str(hours) +" minutes, "
-			uptime %= 60
-		result += str(uptime) +" seconds. "
-
+		from seen import time2str
+		start_time = database.doSQL("SELECT data FROM data WHERE " + \
+					    "type = 'uptime' and " + \
+					    "created_by ='" + str(self.pid) + \
+					    "'")[0][0]
+		result = "I've been awake for " + time2str(int(start_time))
 
 		return Event("privmsg", "", self.return_to_sender(args), [ result ])
 
+class date(MooBotModule):
+	def __init__(self):
+		self.regex = "^date( (utc|rfc|iso))?$"
+
+	def handler(self, **args):
+		import os
+		from irclib import Event
+
+		input = args["text"].split()
+		try:
+			arg = input[2]
+			if (arg == "utc"):
+				cline = " --universal"
+			elif (arg == "rfc"):
+				cline = " --rfc-822"
+			elif (arg == "iso"):
+				cline = " --iso-8601=seconds"
+		except IndexError:
+			cline = ""
+
+		dateprog = os.popen("/bin/date" + cline)
+		datestr = dateprog.read()
+		return Event("privmsg", "", self.return_to_sender(args), \
+			     [ datestr ])
+
+class ddate(MooBotModule):
+	def __init__(self):
+		self.regex = "^ddate$"
+
+	def handler(self, **args):
+		import os
+		from irclib import Event
+
+		if os.path.exists("/usr/bin/ddate"):
+			ddate = os.popen("/usr/bin/ddate '+Today is %{%A, the %e day of %B%}, YOLD %Y. %NCelebrate %H.'")
+			message = ddate.read()
+		else:
+			message = "This aneristic computer doesn't have ddate installed..."
+
+		return Event("privmsg", "", self.return_to_sender(args), \
+			     [ message ])
+	
+
+class xday(MooBotModule):
+	def __init__(self):
+		self.regex = "^xday$"
+
+	def handler(self, **args):
+		import os
+		from irclib import Event
+
+		if os.path.exists("/usr/bin/ddate"):
+			ddate = os.popen("/usr/bin/ddate '+%X days until X-Day. %.'")
+			message = ddate.read()
+		else:
+			message = "This pink computer doesn't have ddate installed..."
+
+		return Event("privmsg", "", self.return_to_sender(args), \
+			     [ message ])
