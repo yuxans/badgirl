bc058ed00424689a16a281a01cb26390a41131d0
FKtPp <kai.fktpp@gmail.com>
2005-03-12 17:46:56 +0000

    test CVSHeader

--- weblookup.py
+++ weblookup.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2005 by baa
 # Copyright (C) 2005 by FKtPp
 #
-# $CVSHeader$
+# $CVSHeader
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
