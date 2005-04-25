698d8051f0bc17af7a9827ca9831b8cff6b210ea
FKtPp <kai.fktpp@gmail.com>
2005-04-25 16:32:32 +0000

    myp ==> standalone class with a __init__ method output_long output_short output style added

--- stocknc.py
+++ stocknc.py
@@ -1,4 +1,5 @@
 # grabs stock quotes
+# -*- coding:gbk -*-
 
 # Copyright (c) 2002 Brett Kelly
 # Copyright (C) 2004 by FKtPp
@@ -19,8 +20,13 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
-from moobot_module import MooBotModule
 import re
+import httplib, urllib
+import urllib2, string, sys
+from irclib import Event 
+from HTMLParser2 import HTMLParser
+from moobot_module import MooBotModule
+
 handler_list = ["stockQuote"]
 
 class stockQuote(MooBotModule):
@@ -32,7 +38,7 @@ class stockQuote(MooBotModule):
 		self.regex="^(em|stockquote) .+"
 
 	def handler(self, **args):
-		from irclib import Event 
+
 		target = self.return_to_sender(args)
 		requested_code = args["text"].split()[2]
 		quote_method = args["text"].split()[1]
@@ -44,8 +50,6 @@ class stockQuote(MooBotModule):
 		return Event("privmsg", "", target, [quote])
 
 	def quote_cn_yahoo(self, symbol):
-		import urllib2, string, sys
-
 		if self.ss.match(symbol):
 			mode = '&m=c'
 		elif self.sz.match(symbol):
@@ -64,18 +68,22 @@ class stockQuote(MooBotModule):
 			sys.exit()
 		splitquote = quote.decode("gbk", "replace").split(',')
 		if splitquote[1] != "0.00":    
-			result = "The current price of %s(%s) is %s" % (splitquote[0].strip('"').rstrip(), (splitquote[1])[1:-4] , splitquote[2])
+			result = "The current price of %s(%s) is %s" % \
+			    (splitquote[0].strip('"').rstrip(), \
+			     (splitquote[1])[1:-4], \
+			     splitquote[2])
 		else:
 			result = "Sorry, I couldn't find that one"
 
 		return result
 
 	def eastmoney(self, symbol):
-
+		__style = 's'
 		err_msg =  "Please use formal STOCK ID to query, exp. 600000 or 000001. Or you can view this link http://quote.eastmoney.com/q.asp?StockCode=%s" % symbol
+
 		if not (self.ss.match(symbol) or self.sz.match(symbol)):
 			return err_msg
-		import httplib, urllib
+
 		# construct and send the query
 		params = urllib.urlencode({"StockCode": symbol.encode("gbk")})
 		headers = {"Content-type": "application/x-www-form-urlencoded",
@@ -87,65 +95,76 @@ class stockQuote(MooBotModule):
 		if response.status != 200:
 			return response.reason
 		else:
-			# construct the result message
-			# example.
-			# ,------( stockname[code] )
-			# | blah blah blah
-			# | foo bar
-			# `------
-			from HTMLParser2 import HTMLParser
-			class myp(HTMLParser):
-				"test class"
-				ignore_data = True
-				data_list = []
-				result = ""
-				__newline = "\r\n| "
-				__data_length = 25
-
-				def handle_starttag(self, tag, attrs):
-					tag = tag.lower()
-					if tag == "script":
-						self.ignore_data = True
-
-				def handle_data(self, data):
-					emptyre = re.compile("^\s*\r\n")
-					if  not self.ignore_data:
-						if len(self.data_list) >= self.__data_length:
-							self.ignore_data = True
-						else:
-							if not emptyre.match(data):
-								data = data.replace("&nbsp;", " ")
-								self.data_list.append(data)
-
-				def handle_endtag(self, tag):
-					tag = tag.lower()
-					if tag == "script":
-						self.ignore_data = False
-
-				def get_result(self):
-					result = self.result
-					result += ",------( %s )" % self.data_list[0] + self.__newline
-					i = 1
-					while i < self.__data_length - 1:
-						column = i % 4
-						if column == 1:
-							result += self.data_list[i].ljust(6)
-						elif column == 2:
-							result += self.data_list[i].rjust(8) + "     "
-						elif column == 3:
-							result += self.data_list[i].ljust(6)
-						else:
-							result += self.data_list[i].rjust(8) + self.__newline
-						i += 1
-					result +=  self.data_list[self.__data_length - 1].rjust(8) + "\r\n`------"
-					return result
-
-			err_re = re.compile(".*您输入的股票代码错误!.*".decode("gbk", "replace"))
+			err_re = re.compile(".*您输入的股票代码错误!.*".decode('gbk','replace'))
 			html = response.read().decode("gbk", "replace")
 			if err_re.search(html):
 				return err_msg
 			else:
-				# parse the response html page
 				p = myp()
 				p.feed(html)
-				return p.get_result()
+				return self.output_short(p.get_result())
+
+	def output_long(self, data_list):
+		__newline = "\r\n | "
+		__data_length = 25
+		result = " ,------( %s )" % data_list[0]
+		result += __newline
+		i = 1
+		while i < __data_length - 1:
+			column = i % 4
+			if column == 1:
+				result += data_list[i].ljust(6)
+			elif column == 2:
+				result += data_list[i].rjust(8)
+				result += "     "
+			elif column == 3:
+				result += data_list[i].ljust(6)
+			else:
+				result += data_list[i].rjust(8)
+				result += __newline
+			i += 1
+		result +=  data_list[__data_length - 1].rjust(8)
+		result += "\r\n `------"
+		return result
+
+	def output_short(self, data_list):
+		result = data_list[0]
+		result += "：%s".decode('gbk','replace') % data_list[2]
+		if '-' in data_list[6]:
+			result += "↓%s(%s)".decode('gbk','replace')
+		else:
+			result += "↑%s(%s)".decode('gbk','replace')
+		result = result % (data_list[6], data_list[10])
+		return result
+
+class myp(HTMLParser):
+	"Html Parser for eastmoney quick stockquote"
+	
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.ignore_data = True
+		self.data_list = []
+
+	def handle_starttag(self, tag, attrs):
+		tag = tag.lower()
+		if tag == "script":
+			self.ignore_data = True
+
+	def handle_data(self, data):
+		__data_length = 25
+		emptyre = re.compile("^\s*\r\n")
+		if  not self.ignore_data:
+			if len(self.data_list) >= __data_length:
+				self.ignore_data = True
+			else:
+				if not emptyre.match(data):
+					data = data.replace("&nbsp;", " ")
+					self.data_list.append(data)
+
+	def handle_endtag(self, tag):
+		tag = tag.lower()
+		if tag == "script":
+			self.ignore_data = False
+
+	def get_result(self):
+		return self.data_list
