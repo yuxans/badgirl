e397fde846f1874d71a6aa71af2306d4d849508a
FKtPp <kai.fktpp@gmail.com>
2006-12-25 15:02:39 +0000

    m3rry (hr157m45 |337

--- translate.py
+++ translate.py
@@ -9,7 +9,7 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
@@ -20,54 +20,100 @@
 from irclib import Event
 from irclib import Event
 from moobot_module import MooBotModule
-handler_list = ["rot13", "reverse", "decode"]
-
-class rot13(MooBotModule):
-	def __init__(self):
-		self.regex = "^rot13 .+$"
-
-	def handler(self, **args):
-		"Qbrf n fvzcyr ebg13, abguvat snapl"
-		import string
-	
-		msg = " ".join(args["text"].split(" ")[2:])
-		newstring = msg.encode("rot_13")
-		return Event("privmsg", "", self.return_to_sender(args), [newstring])
+handler_list = ["reverse", "coding", "leet"]
 
 class reverse(MooBotModule):
-	def __init__(self):
-		self.regex = "^reverse .+"
+    def __init__(self):
+        self.regex = "^reverse .+"
 
-	def handler(self, **args):
-		"gnirts a sesreveR"
-	
-		from string import join
-		orig_string = join(args["text"].split(" ")[2:])
-		newstring = ""
-		for i in range(1, len(orig_string)+1):
-			newstring += orig_string[-i]
-		return Event("privmsg", "", self.return_to_sender(args), [newstring])
+    def handler(self, **args):
+        "gnirts a sesreveR"
+    
+        from string import join
+        orig_string = join(args["text"].split(" ")[2:])
+        newstring = ""
+        for i in range(1, len(orig_string)+1):
+            newstring += orig_string[-i]
+        return Event("privmsg", "", self.return_to_sender(args), [newstring])
 
-class decode(MooBotModule):
-	def __init__(self):
-		self.regex = "^(?:decode from \S+ .+)$"
+class coding(MooBotModule):
+    def __init__(self):
+        self.regex = "^(rot13|u?(hex|base64)) .+"
 
-	def handler(self, **args):
-		(cmd, encodings) = args['text'].split(' ', 4)[2:4]
-		if cmd.lower() == 'from':
-			rawmsg = args['event'].rawdata().split(' ', 6)[6]
-			encodings = encodings.lower().split(":")
-			msg = rawmsg
-			for encoding in encodings:
-				if encoding == 'base64':
-					try:
-						msg = msg.decode(encoding)
-					except:
-						msg = "decode from %s failed" % encoding
-						break
-				else:
-					msg = msg.decode(encoding, "replace")
-		else:
-			msg = "not implemented"
+    def handler(self, **args):
 
-		return Event("privmsg", "", self.return_to_sender(args), [ msg ])
+        cmd, msg = args["text"].split(" ", 2)[1:]
+
+        if cmd.startswith("u"):
+            cmd = cmd.lstrip("u")
+            msg = msg.decode(cmd).decode('utf8')
+        else:
+            msg = msg.encode('utf8').encode(cmd)
+
+        return Event("privmsg", "", 
+                     self.return_to_sender(args), [ msg ])
+
+class leet(MooBotModule):
+    leetCharMap={"A":"/_\\", "B":"b", "C":"(", "D":"|)", "E":"e",
+                 "F":"F", "G":"6", "H":"|-|", "I":"eye", "J":"j",
+                 "K":"|{", "L":"L", "M":"|\\/|", "N":"|\\|",
+                 "O":"()", "P":"P", "Q":"q", "R":"R", "S":"$",
+                 "T":"7", "U":"|_|", "V":"\\/", "W":"\\/\\/",
+                 "X":"><", "Y":"y", "Z":"z", "a":"4", "b":"8",
+                 "c":"(", "d":"d", "e":"3", "f":"f", "g":"6",
+                 "h":"h", "i":"1", "j":"j", "k":"k", "l":"|",
+                 "m":"m", "n":"N", "o":"0", "p":"p", "q":"q",
+                 "r":"r", "s":"5", "t":"7", "u":"U", "v":"V",
+                 "w":"w", "x":"X", "y":"y", "z":"Z", " ":" "}
+    leetWordMap={"you":"j00", "the":"teh", "your":"j00r",
+                 "ever":"evar", "sucks":"soxz", "rocks":"roxz"}
+
+    def __init__(self):
+        self.regex = "^u?1337 .+"
+
+    def handler(self, **args):
+
+        cmd, msg = args["text"].split(" ", 2)[1:]
+
+        if cmd.startswith("u"):
+            msg = "LEET decode wasn't implemented yet"
+        else:
+            msg = self.leet_msg(msg)
+
+        return Event("privmsg", "", self.return_to_sender(args), [ msg ])
+
+    def leet_msg(self, msg):
+        """Translate whole sentence to leet speaking
+
+        Return the translated sentence.
+        """
+        word_list = []
+        for word in msg.split(" "):
+            tmp_word = self.leet_word(word)
+            if not tmp_word:
+                tmp_word = self.leet_char(word)
+            word_list.append(tmp_word)
+        return " ".join(word_list)
+
+    def leet_word(self, word):
+        """Translate a word to leet speaking by word
+
+        return the leet word if successed, or return None"""
+        if self.leetWordMap.has_key(word):
+            return self.leetWordMap[word]
+        else:
+            return None
+
+    def leet_char(self, word):
+        """Translate a word to leet speaking by char
+
+        return the leet word"""
+        char_list = []
+        for char in word:
+            if self.leetCharMap.has_key(char):
+                char_list.append(self.leetCharMap[char])
+            else:
+                char_list.append(char)
+        return "".join(char_list)
+
+# vim:set ai sw=4 expandtab ts=4:
