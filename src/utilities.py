5aa7f0775a35369c16d47139bf8e05f6afa47b48
Xuefer <xuefer@gmail.com>
2007-08-27 12:59:45 +0000

    added trim/ltrim/rtrim

--- src/utilities.py
+++ src/utilities.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """utilities.py - Functions used in moobot modules that may useful elsewhere,
 and don't really belong in a specific module."""
 
@@ -10,3 +11,39 @@ PURPLE = ESCAPE + "[35m"
 NORMAL = ESCAPE + "[0m"
 UNDERLINE = ESCAPE + "[4m"
 BLINK = ESCAPE + "[5m"
+
+import re
+trimRegex = re.compile("^\s+|\s+$")
+ltrimRegex = re.compile("^\s+")
+rtrimRegex = re.compile("\s+$")
+
+def trim(s):
+	"""
+	>>> from utilities import *
+	>>> trim("  -  ")
+	'-'
+	"""
+	return trimRegex.sub("", s)
+
+def ltrim(s):
+	"""
+	>>> from utilities import *
+	>>> ltrim("  -  ")
+	'-  '
+	"""
+	return ltrimRegex.sub("", s)
+
+def rtrim(s):
+	"""
+	>>> from utilities import *
+	>>> rtrim("  -  ")
+	'  -'
+	"""
+	return rtrimRegex.sub("", s)
+
+def _test():
+	import doctest
+	doctest.testmod()
+
+if __name__ == "__main__":
+	_test()
