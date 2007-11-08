c2d4a8ef4c7c7c56d6e1f32b7b6f5f3e52e2081c
FKtPp <kai.fktpp@gmail.com>
2007-11-08 14:42:17 +0000

    so called russian stylize

--- src/translate.py
+++ src/translate.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -*- coding:utf-8 -*-
 
 # Copyright (c) 2002 Daniel DiPaolo, et. al.
+# Copyright (c) 2006, 2007 FKtPp
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,10 +18,11 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+import random
 from irclib import Event
 from irclib import Event
 from moobot_module import MooBotModule
-handler_list = ["reverse", "coding", "leet"]
+handler_list = ["reverse", "coding", "leet", "russian_style"]
 
 class reverse(MooBotModule):
     def __init__(self):
@@ -114,5 +116,96 @@ class leet(MooBotModule):
             else:
                 char_list.append(char)
         return "".join(char_list)
+
+class russian_style (MooBotModule):
+    """Graphic designers sometimes employ faux Cyrillic typography to
+    give a Soviet or Russian feel to text, by replacing Latin letters
+    with Cyrillic letters resembling them in appearance. A simple way
+    to accomplish this is to replace capital letters R and N with
+    Cyrillic Я and И for some"ЯUSSIAИ flavour". Other examples
+    include Ш for W, Ц for U, Г for r, Ф for O, Д for A,
+    and Ч or У for Y.
+    """
+
+    # TODO: fill in more cyrillic characters
+    cyrillic_map = {u'R':u'Я', u'N':u'И', u'W':u'Ш', u'U':u'Ц', u'r':u'Г',
+                    u'O':u'Ф', u'A':u'Д', u'Y':u'ЧУ'}
+    
+    def __init__ (self):
+        self.regex = "^u?ru .+"
+
+    def handler (self, **args):
+
+        cmd, msg = args["text"].split(" ", 2)[1:]
+
+        if cmd.startswith('u'):
+            ascii2cyrillic = False
+
+        newmsg = []
+
+        for each_char in msg:
+            newmsg.append(self.trans_char(each_char, ascii2cyrillic))
+
+        newmsg = ''.join(newmsg)
+
+        return Event("privmsg", "", self.return_to_sender(args), [ newmsg ])
+    
+    def trans_char (self, source_char, ascii2cyrillic):
+        """translate character acroding the translation map.
+
+        source_char: the character(in unicode encoding) to be translated
+        ascii2cyrillic: True for ascii to cyrillic translate, False
+        for translate back.
+
+        returns the translated character (in unicode encoding).
+
+        >>> from translate import *
+        >>> ru = russian_style()
+        >>> ru.trans_char(u'A', True)
+        u'\u0414'
+        >>> ru.trans_char(u'\u0414', False)
+        u'A'
+        >>> ru.trans_char(u'\u0427', False)
+        u'Y'
+        >>> ru.trans_char(u'\u0423', False)
+        u'Y'
+        >>> ru.trans_char(u'X', True)
+        u'X'
+        >>> ru.trans_char(u'X', False)
+        u'X'
+        """
+        dest_char = ''
+
+        if ascii2cyrillic:
+            # translate to cyrillic
+            if self.cyrillic_map.has_key(source_char):
+                maybe_multi_char = self.cyrillic_map[source_char]
+
+                lindex = len (maybe_multi_char) - 1
+                
+                dest_char = lindex \
+                            and maybe_multi_char[random.randint(0,lindex)] \
+                            or maybe_multi_char
+            else:
+                dest_char = source_char
+
+        else:
+            # translate back to ascii
+            for k in self.cyrillic_map.keys():
+                if source_char in self.cyrillic_map[k]:
+                    dest_char = k
+                    break
+
+            if not dest_char:
+                dest_char = source_char
+                
+        return dest_char
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    _test()
 
 # vim:set ai sw=4 expandtab ts=4:
