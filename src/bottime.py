e5dcdf76bc81f823463d99d5dc14bc7654860b8e
FKtPp <kai.fktpp@gmail.com>
2008-02-07 17:12:50 +0000

    sorry, I didn't do any test on this piece of code.

--- src/bottime.py
+++ src/bottime.py
@@ -3,6 +3,9 @@
 # Copyright (c) 2002 Brad Stewart
 # Portions taken from Joe Wreschnig's DateTime.pm for Funbot.
 #
+# Copyright (C) 2008 by FKtPp
+# 
+# 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -10,7 +13,7 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
@@ -19,8 +22,14 @@
 #
 
 """bottime.py - time-related modules """
+import os
+import time
+import datetime
+
+import database
 from moobot_module import MooBotModule
-handler_list=["cputime", "uptime", "date", "ddate", "xday"]
+
+handler_list=["cputime", "uptime", "date"]
 
 class cputime(MooBotModule):
     def __init__(self):
@@ -42,7 +51,6 @@ class cputime(MooBotModule):
 
 class uptime(MooBotModule):
     def __init__(self):
-        import os, database, time
         self.regex = "^uptime$"
         self.pid = os.getpid()
 
@@ -56,7 +64,6 @@ class uptime(MooBotModule):
                        "'uptime', '" + str(self.pid) + "')")
 
     def handler(self, **args):
-        import database
         from irclib import Event
         from seen import time2str
         start_time = database.doSQL("SELECT data FROM data WHERE " + \
@@ -69,60 +76,142 @@ class uptime(MooBotModule):
 
 class date(MooBotModule):
     def __init__(self):
-        self.regex = "^date( (utc|rfc|iso))?$"
+        self.regex = "^date(?:stz)?(?: [^ \t]+)?$"
+        self.help_message_date = ""
+        self.help_message_datestz = ""
 
     def handler(self, **args):
         import os
         from irclib import Event
 
-        input = args["text"].split()
+        input = args["text"].lower().split().remove(0)
+        request_nick = self.return_to_sender(args, select='nick')
+
+        if input[0] == 'datestz':n
+            result = self.handle_tz(input, request_nick)
+        else:
+            result = self.get_time(input, request_nick)
+
+        return Event("privmsg", "", self.return_to_sender(args), \
+                 [ result ])
+
+    def get_time(self, input, request_nick):
+        """return the requested time string
+
+        .valid input list format:
+        --------------
+        [0]  [1]
+        --------------
+        date nick
+        date +/-offset
+        date help
+        date
+        --------------
+        """
+        assert input[0] == "date"
+
+        tz_offset = None
+        qnick = None
+
+        if len(input) == 1:
+            # No Parameter, we assume the user want to know his local time
+            qnick = self.nick_validate(request_nick)
+
+        elif len(input) == 2:
+            tz_offset = self.tz_validate(input[1])
+            if input[1] == "help":
+                return self.help_message_date
+            # if input[1] is not a valid tz_offset string, we assume it is 
+            # a nick
+            elif not tz_offset:
+                qnick = self.nick_validate(input[1])
+        else:
+            return self.help_message_date
+
+        if qnick:
+            tz_offset1 = database.doSQL("select tz_offset from bottime "\
+                                            "where nick = %s" % qnick)
+        if tz_offset1:
+            tz_offset = tz_offset1
+        elif not tz_offset:
+            return time.asctime()
+        
+        return = time.asctime(
+            time.localtime(
+                time.time() + (60 * 60 * tz_offset)
+                )
+            )
+
+
+    def handle_tz(self, input, request_nick):
+        """change or reset the time zone setting
+
+        .valid input list format
+        ----------------------
+        [0]     [1]  [2]
+        ----------------------
+        datestz nick +/-offset
+        datestz +/-offset
+        ----------------------
+        """
+        assert input[0] == "datestz"
+
+        tz_offset = None
+        qnick = self.nick_validate(request_nick)
+
+        if len(input) == 2:
+            tz_offset = tz_validate(input[1])
+        elif len(input) == 3:
+            tz_offset = tz_validate(input[2])
+            qnick = self.nick_validate(input[1])
+
+        if not tz_offset:
+            return self.help_message_datestz
+        else:
+            tmp_data = database.doSQL("select nick, tz_offset from bottime "\
+                                          "where nick = %s" % qnick)
+
+            if tmp_data:
+                sql_string = "update bottime "\
+                    "set tz_offset = %s "\
+                    "where nick = %s" % (tz_offset, qnick)
+            else:
+                sql_string = "insert into bottime (nick, tz_offset) "\
+                    "values (%s, %s)" % (qnick, tz_offset)
+
+            database.doSQL(sql_string)
+
+        return "done!"
+
+    def tz_validate(self, tz_string):
+        """validate the time zone offset string
+
+        return the crosponding time zone offset if the tz_string is
+        valid, or return False.
+        """
+        tz_offset = 0
+        
         try:
-            arg = input[2]
-            if (arg == "utc"):
-                cline = " --universal"
-            elif (arg == "rfc"):
-                cline = " --rfc-822"
-            elif (arg == "iso"):
-                cline = " --iso-8601=seconds"
-        except IndexError:
-            cline = ""
+            tz_offset = int(tz_string)
+        except ValueError:
+            return False
 
-        dateprog = os.popen("/bin/date" + cline)
-        datestr = dateprog.read()
-        return Event("privmsg", "", self.return_to_sender(args), \
-                 [ datestr ])
+        if abs(tz_offset) > 12:
+            return False
 
-class ddate(MooBotModule):
-    def __init__(self):
-        self.regex = "^ddate$"
+        return tz_offset
 
-    def handler(self, **args):
-        import os
-        from irclib import Event
+    def nick_validate(self, nick_string):
+        """validate nick_string to prevent being SQL injected
 
-        if os.path.exists("/usr/bin/ddate"):
-            ddate = os.popen("/usr/bin/ddate '+Today is %{%A, the %e day of %B%}, YOLD %Y. %NCelebrate %H.'")
-            message = ddate.read()
-        else:
-            message = "This aneristic computer doesn't have ddate installed..."
+        XXX this is a dummy function.
+        """
+        return nick_string
+            
+def _test():
+    import doctest
+    doctest.testmod()
 
-        return Event("privmsg", "", self.return_to_sender(args), \
-                 [ message ])
+if __name__ == "__main__":
+    _test()
     
-
-class xday(MooBotModule):
-    def __init__(self):
-        self.regex = "^xday$"
-
-    def handler(self, **args):
-        import os
-        from irclib import Event
-
-        if os.path.exists("/usr/bin/ddate"):
-            ddate = os.popen("/usr/bin/ddate '+%X days until X-Day. %.'")
-            message = ddate.read()
-        else:
-            message = "This pink computer doesn't have ddate installed..."
-
-        return Event("privmsg", "", self.return_to_sender(args), \
-                 [ message ])
