b4aafabb147a13cf125b9410c8a1b9965042f0d4
FKtPp <kai.fktpp@gmail.com>
2008-02-07 03:01:08 +0000

    change indent width to 4 spc

--- src/bottime.py
+++ src/bottime.py
@@ -10,7 +10,7 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
@@ -23,106 +23,106 @@ from moobot_module import MooBotModule
 handler_list=["cputime", "uptime", "date", "ddate", "xday"]
 
 class cputime(MooBotModule):
-	def __init__(self):
-		self.regex = "^cputime$"
+    def __init__(self):
+        self.regex = "^cputime$"
 
-	def handler(self, **args):
-		"""Reports CPU time usage for the current thread."""
-		from os import times
-		from irclib import Event
-		target=args["channel"]
-		if args["type"] == "privmsg":
-			from irclib import nm_to_n
-			target=nm_to_n(args["source"])
-		return Event("privmsg", "", target,
-			     ["User time: " + str(times()[0]) + " seconds, " + \
-			      "system time: " + str(times()[1]) + " seconds.  "+\
-			      "Childrens' user time: " + str(times()[2]) + ", "+\
-			      "childrens' system time: " + str(times()[3])])
+    def handler(self, **args):
+        """Reports CPU time usage for the current thread."""
+        from os import times
+        from irclib import Event
+        target=args["channel"]
+        if args["type"] == "privmsg":
+            from irclib import nm_to_n
+            target=nm_to_n(args["source"])
+        return Event("privmsg", "", target,
+                 ["User time: " + str(times()[0]) + " seconds, " + \
+                  "system time: " + str(times()[1]) + " seconds.  "+\
+                  "Childrens' user time: " + str(times()[2]) + ", "+\
+                  "childrens' system time: " + str(times()[3])])
 
 class uptime(MooBotModule):
-	def __init__(self):
-		import os, database, time
-		self.regex = "^uptime$"
-		self.pid = os.getpid()
+    def __init__(self):
+        import os, database, time
+        self.regex = "^uptime$"
+        self.pid = os.getpid()
 
-		database.doSQL("DELETE FROM data WHERE type = 'uptime' AND " + \
-			       "created_by != '" + str(self.pid) + "'");
-		if len(database.doSQL("SELECT * FROM data WHERE " + \
-				      "type = 'uptime' AND " + \
-				      "created_by ='" + str(self.pid) + "'")) == 0:
-			database.doSQL("INSERT INTO data (data, type, created_by) "+\
-				       "VALUES('" + str(int(time.time())) + "', "+\
-				       "'uptime', '" + str(self.pid) + "')")
+        database.doSQL("DELETE FROM data WHERE type = 'uptime' AND " + \
+                   "created_by != '" + str(self.pid) + "'");
+        if len(database.doSQL("SELECT * FROM data WHERE " + \
+                      "type = 'uptime' AND " + \
+                      "created_by ='" + str(self.pid) + "'")) == 0:
+            database.doSQL("INSERT INTO data (data, type, created_by) "+\
+                       "VALUES('" + str(int(time.time())) + "', "+\
+                       "'uptime', '" + str(self.pid) + "')")
 
-	def handler(self, **args):
-		import database
-		from irclib import Event
-		from seen import time2str
-		start_time = database.doSQL("SELECT data FROM data WHERE " + \
-					    "type = 'uptime' and " + \
-					    "created_by ='" + str(self.pid) + \
-					    "'")[0][0]
-		result = "I've been awake for " + time2str(int(start_time))
+    def handler(self, **args):
+        import database
+        from irclib import Event
+        from seen import time2str
+        start_time = database.doSQL("SELECT data FROM data WHERE " + \
+                        "type = 'uptime' and " + \
+                        "created_by ='" + str(self.pid) + \
+                        "'")[0][0]
+        result = "I've been awake for " + time2str(int(start_time))
 
-		return Event("privmsg", "", self.return_to_sender(args), [ result ])
+        return Event("privmsg", "", self.return_to_sender(args), [ result ])
 
 class date(MooBotModule):
-	def __init__(self):
-		self.regex = "^date( (utc|rfc|iso))?$"
+    def __init__(self):
+        self.regex = "^date( (utc|rfc|iso))?$"
 
-	def handler(self, **args):
-		import os
-		from irclib import Event
+    def handler(self, **args):
+        import os
+        from irclib import Event
 
-		input = args["text"].split()
-		try:
-			arg = input[2]
-			if (arg == "utc"):
-				cline = " --universal"
-			elif (arg == "rfc"):
-				cline = " --rfc-822"
-			elif (arg == "iso"):
-				cline = " --iso-8601=seconds"
-		except IndexError:
-			cline = ""
+        input = args["text"].split()
+        try:
+            arg = input[2]
+            if (arg == "utc"):
+                cline = " --universal"
+            elif (arg == "rfc"):
+                cline = " --rfc-822"
+            elif (arg == "iso"):
+                cline = " --iso-8601=seconds"
+        except IndexError:
+            cline = ""
 
-		dateprog = os.popen("/bin/date" + cline)
-		datestr = dateprog.read()
-		return Event("privmsg", "", self.return_to_sender(args), \
-			     [ datestr ])
+        dateprog = os.popen("/bin/date" + cline)
+        datestr = dateprog.read()
+        return Event("privmsg", "", self.return_to_sender(args), \
+                 [ datestr ])
 
 class ddate(MooBotModule):
-	def __init__(self):
-		self.regex = "^ddate$"
+    def __init__(self):
+        self.regex = "^ddate$"
 
-	def handler(self, **args):
-		import os
-		from irclib import Event
+    def handler(self, **args):
+        import os
+        from irclib import Event
 
-		if os.path.exists("/usr/bin/ddate"):
-			ddate = os.popen("/usr/bin/ddate '+Today is %{%A, the %e day of %B%}, YOLD %Y. %NCelebrate %H.'")
-			message = ddate.read()
-		else:
-			message = "This aneristic computer doesn't have ddate installed..."
+        if os.path.exists("/usr/bin/ddate"):
+            ddate = os.popen("/usr/bin/ddate '+Today is %{%A, the %e day of %B%}, YOLD %Y. %NCelebrate %H.'")
+            message = ddate.read()
+        else:
+            message = "This aneristic computer doesn't have ddate installed..."
 
-		return Event("privmsg", "", self.return_to_sender(args), \
-			     [ message ])
-	
+        return Event("privmsg", "", self.return_to_sender(args), \
+                 [ message ])
+    
 
 class xday(MooBotModule):
-	def __init__(self):
-		self.regex = "^xday$"
+    def __init__(self):
+        self.regex = "^xday$"
 
-	def handler(self, **args):
-		import os
-		from irclib import Event
+    def handler(self, **args):
+        import os
+        from irclib import Event
 
-		if os.path.exists("/usr/bin/ddate"):
-			ddate = os.popen("/usr/bin/ddate '+%X days until X-Day. %.'")
-			message = ddate.read()
-		else:
-			message = "This pink computer doesn't have ddate installed..."
+        if os.path.exists("/usr/bin/ddate"):
+            ddate = os.popen("/usr/bin/ddate '+%X days until X-Day. %.'")
+            message = ddate.read()
+        else:
+            message = "This pink computer doesn't have ddate installed..."
 
-		return Event("privmsg", "", self.return_to_sender(args), \
-			     [ message ])
+        return Event("privmsg", "", self.return_to_sender(args), \
+                 [ message ])
