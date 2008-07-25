0caf5a57fb7d5c962d91b5d29cb400b2d3eae008
Xuefer <xuefer@gmail.com>
2008-07-25 07:11:25 +0000

    fix indention

--- src/geoip.py
+++ src/geoip.py
@@ -31,122 +31,122 @@ class IEURLopener(urllib.FancyURLopener):
 urllib._urlopener = IEURLopener()
 
 class chunzhen(MooBotModule):
-    """Get geographic location infomation of specific IP address from web.
+	"""Get geographic location infomation of specific IP address from web.
 
-    Query the infomation from http://www.cz88.net/ server, and pasre,
-    print the result.
-    """
+	Query the infomation from http://www.cz88.net/ server, and pasre,
+	print the result.
+	"""
     
-    def __init__(self):
-        """
-	>>> import re, geoip
-	>>> a = geoip.chunzhen()
-	>>> r = re.compile(a.regex)
-	>>> r.match('geoip ') and True or False
-	False
-	>>> r.match('geoip a') and True or False
-	False
-	>>> r.match('geoip a.') and True or False
-	False
-	>>> r.match('geoip a.b') and True or False
-	False
-	>>> r.match('geoip 1') and True or False
-	False
-	>>> r.match('geoip 1.') and True or False
-	False
-	>>> r.match('geoip 1.0') and True or False
-	True
-	>>> r.match('geoip 1.1') and True or False
-	True
-	>>> r.match('geoip 1.1.0') and True or False
-	True
-	>>> r.match('geoip 1.1.1') and True or False
-	True
-	>>> r.match('geoip 1.1.1.1') and True or False
-	True
-	>>> r.match('geoip 1.0.0.1') and True or False
-	True
-	>>> r.match('geoip 1.1.1.1.1') and True or False
-	False
-	>>> r.match('geoip 255.1.1.1') and True or False
-	False
-	>>> r.match('geoip 1.255.1.1') and True or False
-	False
-	>>> r.match('geoip 1.1.255.1') and True or False
-	False
-	>>> r.match('geoip 1.1.1.255') and True or False
-	False
-	>>> r.match('geoip 1.1.1.0') and True or False
-	False
-	>>> r.match('geoip 0.1.1.1') and True or False
-	False
-	>>> r.match('geoip 1.1.1.a') and True or False
-	False
-	>>> 
-	"""
-        # ipv4 pattern initialization
-        num0_254 = '(?:25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)'
-	num1_254 = '(?:25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)'
-        ipv4part = '(?:%s(?:\.%s){1,2}(?:\.%s)?)' % (num1_254,
-						     num0_254,
-						     num1_254)
+	def __init__(self):
+		"""
+		>>> import re, geoip
+		>>> a = geoip.chunzhen()
+		>>> r = re.compile(a.regex)
+		>>> r.match('geoip ') and True or False
+		False
+		>>> r.match('geoip a') and True or False
+		False
+		>>> r.match('geoip a.') and True or False
+		False
+		>>> r.match('geoip a.b') and True or False
+		False
+		>>> r.match('geoip 1') and True or False
+		False
+		>>> r.match('geoip 1.') and True or False
+		False
+		>>> r.match('geoip 1.0') and True or False
+		True
+		>>> r.match('geoip 1.1') and True or False
+		True
+		>>> r.match('geoip 1.1.0') and True or False
+		True
+		>>> r.match('geoip 1.1.1') and True or False
+		True
+		>>> r.match('geoip 1.1.1.1') and True or False
+		True
+		>>> r.match('geoip 1.0.0.1') and True or False
+		True
+		>>> r.match('geoip 1.1.1.1.1') and True or False
+		False
+		>>> r.match('geoip 255.1.1.1') and True or False
+		False
+		>>> r.match('geoip 1.255.1.1') and True or False
+		False
+		>>> r.match('geoip 1.1.255.1') and True or False
+		False
+		>>> r.match('geoip 1.1.1.255') and True or False
+		False
+		>>> r.match('geoip 1.1.1.0') and True or False
+		False
+		>>> r.match('geoip 0.1.1.1') and True or False
+		False
+		>>> r.match('geoip 1.1.1.a') and True or False
+		False
+		>>> 
+		"""
+		# ipv4 pattern initialization
+		num0_254 = '(?:25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)'
+		num1_254 = '(?:25[0-4]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)'
+		ipv4part = '(?:%s(?:\.%s){1,2}(?:\.%s)?)' % (num1_254,
+								 num0_254,
+								 num1_254)
 
-        self.regex='^geoip\s+%s$' % (ipv4part)
+		self.regex='^geoip\s+%s$' % (ipv4part)
 
-    def handler(self, **args):
-        query_str = args["text"].split()
+	def handler(self, **args):
+		query_str = args["text"].split()
 
-	query_str = query_str[2].strip()
+		query_str = query_str[2].strip()
 
-	ipaddress = self.fixipv4(query_str)
+		ipaddress = self.fixipv4(query_str)
 
-        parms = urllib.urlencode({'ip':ipaddress})
-        f = urllib.urlopen('http://www.cz88.net/ip/ipcheck.aspx', parms)
+		parms = urllib.urlencode({'ip':ipaddress})
+		f = urllib.urlopen('http://www.cz88.net/ip/ipcheck.aspx', parms)
 
-	geo_re = re.compile('document.write\("(.*)"\);')
+		geo_re = re.compile('document.write\("(.*)"\);')
 
-	m = geo_re.search(f.read().decode('gbk'))
-	f.close()
+		m = geo_re.search(f.read().decode('gbk'))
+		f.close()
 
-        geoinfo = (m and m.group(1) or 'Not Found')
+		geoinfo = (m and m.group(1) or 'Not Found')
 
-        target = self.return_to_sender(args)
-        result = Event('privmsg', '', target, [ ''.join((query_str, ': ', geoinfo))])
+		target = self.return_to_sender(args)
+		result = Event('privmsg', '', target, [ ''.join((query_str, ': ', geoinfo))])
 
-        return result
+		return result
 
-    def fixipv4(self, ipv4string):
-        """fill the 3, 4 part of a particalarly typed ip address.
+	def fixipv4(self, ipv4string):
+		"""fill the 3, 4 part of a particalarly typed ip address.
 
-	fill '.0.1' if it has only two parts, fill '.1' if it has
-	three parts, do nothing if it has all the four parts.
+		fill '.0.1' if it has only two parts, fill '.1' if it has
+		three parts, do nothing if it has all the four parts.
 
-	>>> import geoip
-	>>> a = geoip.chunzhen()
-	>>> a.fixipv4('192.168')
-	'192.168.0.1'
-	>>> a.fixipv4('192.168.2')
-	'192.168.2.1'
-	>>> a.fixipv4('192.168.2.22')
-	'192.168.2.22'
-	>>> 
-	"""
+		>>> import geoip
+		>>> a = geoip.chunzhen()
+		>>> a.fixipv4('192.168')
+		'192.168.0.1'
+		>>> a.fixipv4('192.168.2')
+		'192.168.2.1'
+		>>> a.fixipv4('192.168.2.22')
+		'192.168.2.22'
+		>>> 
+		"""
 
-        parts = ipv4string.split('.')
-        
-        l = len(parts)
+		parts = ipv4string.split('.')
+		
+		l = len(parts)
 
-        if l == 2:
-            parts.extend(['0','1'])
-        elif l == 3:
-            parts.extend(['1'])
-        elif l == 4:
-            pass
-        else:
-            # IP Error, length must be 2, 3, 4 or regexp must be wrong
-            assert False
-	    
-        return '.'.join(parts)
+		if l == 2:
+			parts.extend(['0','1'])
+		elif l == 3:
+			parts.extend(['1'])
+		elif l == 4:
+			pass
+		else:
+			# IP Error, length must be 2, 3, 4 or regexp must be wrong
+			assert False
+		
+		return '.'.join(parts)
 
 def _test():
 	import doctest
